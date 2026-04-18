859beda062a3de2cdbb56e1e528260d17243fe23
Fanani <azfann.natinons@gmail.com>
2026-04-18 20:29:03 +0700

    fix: implement BarangForm schema and fix mass assignment in model

--- app/Filament/Resources/Barangs/Schemas/BarangForm.php
+++ app/Filament/Resources/Barangs/Schemas/BarangForm.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\Barangs\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
 
 class BarangForm
 {
@@ -10,7 +12,29 @@ class BarangForm
     {
         return $schema
             ->components([
-                //
+                Select::make('kategori_id')
+                    ->relationship('kategori', 'kategori_nama')
+                    ->required()
+                    ->searchable(),
+
+                TextInput::make('barang_kode')
+                    ->required()
+                    ->maxLength(10)
+                    ->unique(ignoreRecord: true),
+
+                TextInput::make('barang_nama')
+                    ->required()
+                    ->maxLength(100),
+
+                TextInput::make('harga_beli')
+                    ->numeric()
+                    ->prefix('Rp')
+                    ->required(),
+
+                TextInput::make('harga_jual')
+                    ->numeric()
+                    ->prefix('Rp')
+                    ->required(),
             ]);
     }
 }
