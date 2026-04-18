09e981f5fda410d5701cef7bb04b2850660af66a
Fanani <azfann.natinons@gmail.com>
2026-04-18 22:26:43 +0700

    feat: define hasMany relationship to Stok in Supplier model

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -9,4 +9,9 @@ class Supplier extends Model
     protected $table = 'm_supplier';
     protected $primaryKey = 'supplier_id';
     protected $fillable = ['supplier_kode', 'supplier_nama', 'supplier_alamat'];
+
+    public function stoks()
+    {
+        return $this->hasMany(Stok::class, 'supplier_id', 'supplier_id');
+    }
 }
