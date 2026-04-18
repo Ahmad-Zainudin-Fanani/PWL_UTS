3a3e2ac9e54c047c6f1ff4aa1e3f3b8d93386b33
Fanani <azfann.natinons@gmail.com>
2026-04-18 22:25:05 +0700

    add datetime casting and relationships (User, PenjualanDetail) to Penjualan model

--- app/Models/Penjualan.php
+++ app/Models/Penjualan.php
@@ -9,4 +9,18 @@ class Penjualan extends Model
     protected $table = 't_penjualan';
     protected $primaryKey = 'penjualan_id';
     protected $fillable = ['user_id', 'pembeli', 'penjualan_kode', 'penjualan_tanggal'];
+
+    protected $casts = [
+        'penjualan_tanggal' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    public function details()
+    {
+        return $this->hasMany(PenjualanDetail::class, 'penjualan_id', 'penjualan_id');
+    }
 }
