62fcbf3717368f31667cf30efd2b09f5f5c3a392
Fanani <azfann.natinons@gmail.com>
2026-04-18 22:57:45 +0700

    feat: buat schema LevelsTable untuk menampilkan daftar level user

--- app/Filament/Resources/Levels/Tables/LevelsTable.php
+++ app/Filament/Resources/Levels/Tables/LevelsTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Levels\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class LevelsTable
@@ -13,7 +14,22 @@ class LevelsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('level_kode')
+                    ->label('Kode Level')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('level_nama')
+                    ->label('Nama Level')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
